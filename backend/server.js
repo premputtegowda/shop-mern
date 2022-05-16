7f7cd33dd5b834b9b9e315b14b2ee932e1ce45bf
Prem Puttegowda <pkpgowda@gmail.com>
2022-05-16 10:13:58 -0700

    nodemon and concurrenlty

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,9 @@ app.get('/api/products',(req,res)=> {
 })
 
 app.get('/api/products/:id', (req,res) =>{
-    console.log(req.params.id)
+
     const product = products.find((product)=> product._id === req.params.id)
-    console.log(product)
+   
     res.json(product)
 } )
 app.listen(5000, console.log('Server running on port 5000'))
